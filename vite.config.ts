6e30f50dad4ae7bb9581b1898708c035be34fd84
weilycoder <weily2009@outlook.com>
2026-08-30 10:08:55 +0800

    feat: initialize React application with TypeScript and Vite

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  server: {
+    port: 30147,
+    strictPort: true,
+  },
+});
